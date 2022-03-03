515f3f5e47dee1739e0da89b11d4986194aee742
Daniel Bustos <dbustosg01@educastillalamancha.es>
2022-03-03 16:58:00 +0100

    Subida proyecto
    1º Subida del proyecto

---
+++ EleJones/Assets/Scripts/MovimientoJugadora.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugadora : MonoBehaviour
+{
+    // Variables
+    [Range(1, 10)] public float speed;
+    Rigidbody2D rb2d;
+    SpriteRenderer spRd;
+
+    private Animator animator;
+    bool isJumping = false;
+    [Range(1, 500)] public float potenciaSalto;
+
+    //Variable para puntuacion de powerUp
+    public int puntuacion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        spRd = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float movimientoH = Input.GetAxisRaw("Horizontal");
+        rb2d.velocity = new Vector2(movimientoH * speed, rb2d.velocity.y);
+
+        //Control de la dirección del muñeco
+        if (movimientoH > 0)
+            spRd.flipX = false;
+        else if (movimientoH < 0)
+            spRd.flipX = true;
+
+        //Control de la animación
+        /*if (movimientoH != 0)
+            animator.SetBool("isWalking", true);
+        else
+            animator.SetBool("isWalking", false);*/
+
+        //Para el salto
+        if (Input.GetButton("Jump") && !isJumping)
+        {
+            rb2d.AddForce(Vector2.up * potenciaSalto);
+            isJumping = true;
+        }
+    }
+
+    //Controlará la colisión contra el suelo, para los saltos
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Suelo"))
+        {
+            isJumping = false;
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+        }
+    }
+
+    public void IncrementarPuntos(int cantidad)
+    {
+        puntuacion += cantidad;
+    }
+}
